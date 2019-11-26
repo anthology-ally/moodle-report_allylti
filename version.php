ffc0cde93eebbc25010281053bba94b53e90f8b0
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version      = 2018080801;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires     = 2018120300;        // Requires this Moodle version.
-$plugin->release      = '3.6.3';
+$plugin->requires     = 2019052000;        // Requires this Moodle version.
+$plugin->release      = '3.7.3';
 $plugin->component    = 'report_allylti';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
     'tool_ally'      => 2018080800,
