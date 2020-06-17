a6c63229a24c12d03d57c16f14a069b75053c9a7
David Castro <davidcastro00@gmail.com>
2020-06-17 11:01:56 -0500

    INT-14900: Bump release of Ally for releasing on moodle.org

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version      = 2018080801;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires     = 2018120300;        // Requires this Moodle version.
-$plugin->release      = '3.6.3';
+$plugin->release      = '3.6.4';
 $plugin->component    = 'report_allylti';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
     'tool_ally'      => 2018080800,
