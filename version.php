1b9c145d975c462c733a0746c6cb7b4004e17f15
Rafael Monterroza <rafael.monterroza@openlms.net>
2022-07-29 18:07:24 +0000

    INT-18181: bump versions for 3.11 mp4

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2022060600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version      = 2022072900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires     = 2021051700;        // Requires this Moodle version.
-$plugin->release      = '3.11.3';
+$plugin->release      = '3.11.6';
 $plugin->component    = 'report_allylti';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
-    'tool_ally'      => 2022060600,
-    'filter_ally'    => 2022060600,
+    'tool_ally'      => 2022072900,
+    'filter_ally'    => 2022072900,
 ];
