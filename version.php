34ffde48699345e5722c31915d66c6f77aa0a63c
Andrew Moore <andrew.moore@anthology.com>
2026-03-25 16:12:20 -0400

    Update to only support Moodle 4.5

--- version.php
+++ version.php
@@ -31,6 +31,10 @@ $plugin->release      = '4.5.1';
 $plugin->version      = 2026012800;
 $plugin->requires     = 2024092700;
 $plugin->component    = 'report_allylti';
+$plugin->supported    = [
+    405,
+    405
+];
 $plugin->dependencies = [
     'tool_ally'      => 2026012800,
     'filter_ally'    => 2026012800,
